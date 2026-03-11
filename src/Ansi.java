0b72d906de52976fbd3d9dbf2bfcc6feae541022
Alejandro Gachupin <cyber@gachupin.io>
2026-03-11 17:29:32 -0500

    Implemented.

---
+++ src/Ansi.java
@@ -0,0 +1,22 @@
+/**
+ * Incorporate description herein.
+ * <p>
+ * @author Alejandro Gachupin
+ * @contact gachae25@wfu.edu
+ * @contact cyber@gachupin.io
+ * <p>
+ * @since 2025-03-11s
+ */
+public final class Ansi {
+    public static final String RESET = "\\u001B[0m"; // Incorporate description herein.
+
+    public static final String BOLD = "\\u001B[1m"; // Incorporate description herein.
+    public static final String DIM = "\\u001B[2m"; // Incorporate description herein.
+    public static final String ITALIC = "\\u001B[3m"; // Incorporate description herein.
+    public static final String UNDERLINE = "\\u001B[4m"; // Incorporate description herein.
+
+    /**
+     * Incorporate description herein.
+     */
+    private Ansi() {}
+}
